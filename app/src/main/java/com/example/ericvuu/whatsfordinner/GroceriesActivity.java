a44c46e8edebdb0778177a689b0d7f2bf42c42ad
baconlover51 <vuueric@yahoo.com>
2017-10-22 23:43:34 -0700

    groceriesactivity changes

--- app/src/main/java/com/example/ericvuu/whatsfordinner/GroceriesActivity.java
+++ app/src/main/java/com/example/ericvuu/whatsfordinner/GroceriesActivity.java
@@ -1,41 +1,85 @@
 package com.example.ericvuu.whatsfordinner;
 
+import android.gesture.Gesture;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
+
+import org.w3c.dom.Text;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+import static android.R.id.message;
 
 public class GroceriesActivity extends AppCompatActivity {
-    public TextView groceryname1, groceryname2, groceryname3, groceryname4;
+
+    private static final String TAG ="GroceriesActivity";
+    int amount = 0;
+    int amount2 = 0;
+    int amount3 = 0;
+    int amount4 = 0;
+    int amount5 = 0;
+
+    ArrayAdapter adapter;
+    public TextView textView, textView2, textView3, textView4, textView5;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_groceries);
+        final SwipeMenuListView listView  = (SwipeMenuListView) findViewById(R.id.listView);
 
-
-        groceryname1 = (TextView) findViewById(R.id.grocery1);
-        groceryname2 = (TextView) findViewById(R.id.grocery2);
-        groceryname3 = (TextView) findViewById(R.id.grocery3);
-        groceryname4 = (TextView) findViewById(R.id.grocery4);
-
-        String message;
-        String message2;
         FileInputStream fis1 = null;
         FileInputStream fis2 = null;
         FileInputStream fis3 = null;
         FileInputStream fis4 = null;
+        FileInputStream fis5 = null;
+
+        String message;
+        String message2;
+        String message3;
+        String message4;
+        String message5;
+
+        textView = (TextView) findViewById(R.id.number);
+        textView2 = (TextView) findViewById(R.id.number2);
+        textView3 = (TextView) findViewById(R.id.number3);
+        textView4 = (TextView) findViewById(R.id.number4);
+        textView5 = (TextView) findViewById(R.id.number5);
+
+        ArrayList<String> list = new ArrayList<>();
+
+
         try {
             fis1 = openFileInput("Ingredient1");
+            fis2 = openFileInput("Ingredient2");
+            fis3 = openFileInput("Ingredient3");
+            fis4 = openFileInput("Ingredient4");
+            fis5 = openFileInput("Ingredient5");
         } catch (FileNotFoundException e1) {
             e1.printStackTrace();
         }
+
+        //grocery text 1
         InputStreamReader isr = new InputStreamReader(fis1);
         BufferedReader br = new BufferedReader(isr);
         StringBuffer sb = new StringBuffer();
@@ -46,35 +90,179 @@ public class GroceriesActivity extends AppCompatActivity {
         } catch (IOException e1) {
             e1.printStackTrace();
         }
-        groceryname1.setText(sb.toString());
-        try {
-            fis1.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        list.add(sb.toString());
 
-        try {
-            fis2 = openFileInput("Ingredient2");
-        } catch (FileNotFoundException e1) {
-            e1.printStackTrace();
-        }
+        // grocery text 2
         InputStreamReader isr2 = new InputStreamReader(fis2);
         BufferedReader br2 = new BufferedReader(isr2);
         StringBuffer sb2 = new StringBuffer();
         try {
             while ((message2 = br2.readLine()) != null) {
-                sb.append(message2);
+                sb2.append(message2);
             }
         } catch (IOException e1) {
             e1.printStackTrace();
         }
-        groceryname2.setText(sb2.toString());
+        list.add(sb2.toString());
+
+
+        //grocery text 3
+        InputStreamReader isr3 = new InputStreamReader(fis3);
+        BufferedReader br3 = new BufferedReader(isr3);
+        StringBuffer sb3 = new StringBuffer();
         try {
-            fis2.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+            while ((message3 = br3.readLine()) != null) {
+                sb3.append(message3);
+            }
+        } catch (IOException e1) {
+            e1.printStackTrace();
         }
+        list.add(sb3.toString());
 
 
+        //grocery text 4
+        InputStreamReader isr4 = new InputStreamReader(fis4);
+        BufferedReader br4 = new BufferedReader(isr4);
+        StringBuffer sb4 = new StringBuffer();
+        try {
+            while ((message4 = br4.readLine()) != null) {
+                sb4.append(message4);
+            }
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        list.add(sb4.toString());
+
+
+        //grocery text 5
+        InputStreamReader isr5 = new InputStreamReader(fis5);
+        BufferedReader br5 = new BufferedReader(isr5);
+        StringBuffer sb5 = new StringBuffer();
+        try {
+            while ((message5 = br5.readLine()) != null) {
+                sb5.append(message5);
+            }
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        list.add(sb5.toString());
+
+
+        adapter =  new ArrayAdapter(GroceriesActivity.this, android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
+
+        final SwipeMenuCreator creator = new SwipeMenuCreator() {
+
+            @Override
+            public void create(SwipeMenu menu) {
+                // create "open" item
+                SwipeMenuItem openItem = new SwipeMenuItem(
+                        getApplicationContext());
+                // set item background
+                openItem.setBackground(new ColorDrawable(Color.rgb(0xC9, 0xC9,
+                        0xCE)));
+                // set item width
+                openItem.setWidth(170);
+                // set item title
+                openItem.setTitle("+");
+                // set item title fontsize
+                openItem.setTitleSize(18);
+                // set item title font color
+                openItem.setTitleColor(Color.WHITE);
+                // add to menu
+                menu.addMenuItem(openItem);
+
+                // create "delete" item
+                SwipeMenuItem deleteItem = new SwipeMenuItem(
+                        getApplicationContext());
+                // set item background
+                deleteItem.setBackground(new ColorDrawable(Color.rgb(0xF9,
+                        0x3F, 0x25)));
+                // set item width
+                deleteItem.setWidth(170);
+                // set a icon
+                deleteItem.setIcon(R.mipmap.ic_minus);
+                // add to menu
+                menu.addMenuItem(deleteItem);
+            }
+        };
+
+        // set creator
+        listView.setMenuCreator(creator);
+
+        listView.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
+                switch (index) {
+                    case 0:
+                        // +
+                        Log.d(TAG, "onMenuItemCLick: clicked postion " + position);
+                        if(position == 0)
+                        {
+                            amount++;
+                            textView.setText(String.valueOf(amount));
+                            Log.d(TAG, "onMenuItemCLick: amount " + amount);
+                        }
+                        if(position == 1)
+                        {
+                            amount2++;
+                            textView2.setText(String.valueOf(amount2));
+                            Log.d(TAG, "onMenuItemCLick: amount " + amount2);
+                        }
+                        if(position == 2)
+                        {
+                            amount3++;
+                            textView3.setText(String.valueOf(amount3));
+                            Log.d(TAG, "onMenuItemCLick: amount " + amount3);
+                        }
+                        if(position == 3)
+                        {
+                            amount4++;
+                            textView4.setText(String.valueOf(amount4));
+                            Log.d(TAG, "onMenuItemCLick: amount " + amount4);
+                        }
+                        if(position == 4)
+                        {
+                            amount5++;
+                            textView5.setText(String.valueOf(amount5));
+                            Log.d(TAG, "onMenuItemCLick: amount " + amount5);
+                        }
+
+
+                        break;
+                    case 1:
+                        // -
+                        Log.d(TAG, "onMenuItemCLick: clicked postion " + position);
+                        if(position == 0)
+                        {
+                            amount--;
+                        }
+                        if(position == 1)
+                        {
+                            amount2--;
+                        }
+                        if(position == 2)
+                        {
+                            amount3--;
+                        }
+                        if(position == 3)
+                        {
+                            amount4--;
+                        }
+                        if(position == 4)
+                        {
+                            amount5--;
+                        }
+                        break;
+                }
+                // false : close the menu; true : not close the menu
+                return false;
+            }
+        });
     }
+
 }
+
+
+
+
--- app/src/main/java/com/example/ericvuu/whatsfordinner/NewDishActivity.java
+++ app/src/main/java/com/example/ericvuu/whatsfordinner/NewDishActivity.java
@@ -40,6 +40,7 @@ public class NewDishActivity extends AppCompatActivity implements View.OnClickLi
     String file_name3 = "Ingredient3";
     String file_name4 = "Ingredient4";
     String file_name5 = "Ingredient5";
+    String ingredient1, ingredient2, ingredient3;
 
     public String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/WhatsforDinner";
 
@@ -50,7 +51,6 @@ public class NewDishActivity extends AppCompatActivity implements View.OnClickLi
 
         findViewById(R.id.Addbutton).setOnClickListener(this);
         findViewById(R.id.Savebutton).setOnClickListener(this);
-        findViewById(R.id.loadButton).setOnClickListener(this);
         findViewById(R.id.RecipeName).setOnFocusChangeListener(this);
         imageView = (ImageView) findViewById(R.id.recipeimage);
         recipeText = (EditText) findViewById(R.id.RecipeName);
@@ -59,26 +59,143 @@ public class NewDishActivity extends AppCompatActivity implements View.OnClickLi
         Spinner dropdown3 = (Spinner) findViewById(R.id.ingredient3);
         Spinner dropdown4 = (Spinner) findViewById(R.id.ingredient4);
         Spinner dropdown5 = (Spinner) findViewById(R.id.ingredient5);
-        dropdown1.setOnItemSelectedListener(this);
-        dropdown2.setOnItemSelectedListener(this);
-        dropdown3.setOnItemSelectedListener(this);
-        dropdown4.setOnItemSelectedListener(this);
-        dropdown5.setOnItemSelectedListener(this);
         direction = (EditText) findViewById(R.id.DirectionText);
-        textView = (TextView) findViewById(R.id.loadView);
 
-        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(NewDishActivity.this, android.R.layout.simple_list_item_1,
+        //dropdown1
+         ArrayAdapter<String> adapterIngredient1 = new ArrayAdapter<String>(NewDishActivity.this, android.R.layout.simple_list_item_1,
                 getResources().getStringArray(R.array.names));
-        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        dropdown1.setAdapter(myAdapter);
-        dropdown2.setAdapter(myAdapter);
-        dropdown3.setAdapter(myAdapter);
-        dropdown4.setAdapter(myAdapter);
-        dropdown5.setAdapter(myAdapter);
-        File dir = new File(path);
+        adapterIngredient1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dropdown1.setAdapter(adapterIngredient1);
+        dropdown1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                                                @Override
+                                                public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                                                    String parent = adapterView.getItemAtPosition(i).toString();
+                                                    try {
+                                                        FileOutputStream fos1 = openFileOutput(file_name1, MODE_PRIVATE);
+                                                        fos1.write(parent.getBytes());
+                                                    } catch (FileNotFoundException e) {
+                                                        e.printStackTrace();
+                                                    } catch (IOException e) {
+                                                        e.printStackTrace();
+                                                    }
+
+                                                }
+
+                                                @Override
+                                                public void onNothingSelected(AdapterView<?> adapterView) {
+
+                                                }
+                                            });
+
+        //dropdown2
+        ArrayAdapter<String> adapterIngredient2 = new ArrayAdapter<String>(NewDishActivity.this, android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.names));
+        adapterIngredient2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dropdown2.setAdapter(adapterIngredient2);
+        dropdown2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String parent = adapterView.getItemAtPosition(i).toString();
+                try {
+                    FileOutputStream fos2 = openFileOutput(file_name2, MODE_PRIVATE);
+                    fos2.write(parent.getBytes());
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        //dropdown3
+        ArrayAdapter<String> adapterIngredient3 = new ArrayAdapter<String>(NewDishActivity.this, android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.names));
+        adapterIngredient3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dropdown3.setAdapter(adapterIngredient3);
+        dropdown3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String parent = adapterView.getItemAtPosition(i).toString();
+                try {
+                    FileOutputStream fos3 = openFileOutput(file_name3, MODE_PRIVATE);
+                    fos3.write(parent.getBytes());
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        //dropdown4
+        ArrayAdapter<String> adapterIngredient4 = new ArrayAdapter<String>(NewDishActivity.this, android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.names));
+        adapterIngredient4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dropdown4.setAdapter(adapterIngredient4);
+        dropdown4.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String parent = adapterView.getItemAtPosition(i).toString();
+                try {
+                    FileOutputStream fos4 = openFileOutput(file_name4, MODE_PRIVATE);
+                    fos4.write(parent.getBytes());
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        //dropdown5
+        ArrayAdapter<String> adapterIngredient5 = new ArrayAdapter<String>(NewDishActivity.this, android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.names));
+        adapterIngredient5.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dropdown5.setAdapter(adapterIngredient5);
+        dropdown5.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String parent = adapterView.getItemAtPosition(i).toString();
+                try {
+                    FileOutputStream fos5 = openFileOutput(file_name5, MODE_PRIVATE);
+                    fos5.write(parent.getBytes());
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        //dropdown3.setAdapter(myAdapter);
+        //dropdown4.setAdapter(myAdapter);
+        //dropdown5.setAdapter(myAdapter);
+            File dir = new File(path);
         dir.mkdirs();
 
-    }
+        }
+
+
+
 
     @Override
     public void onClick(View v) {
@@ -89,13 +206,9 @@ public class NewDishActivity extends AppCompatActivity implements View.OnClickLi
         if (i == R.id.Savebutton) {
             String message = recipeText.getText().toString();
             String file_name = "RecipeText";
-
             String file_name6 = "Directions";
-
             try {
-                FileOutputStream fos = openFileOutput(file_name, MODE_PRIVATE);
-                FileOutputStream fos6 = openFileOutput(file_name6, MODE_APPEND);
-                fos.write(message.getBytes() );
+                FileOutputStream fosRecipeText = openFileOutput(file_name, MODE_PRIVATE);
                 Toast.makeText(getApplicationContext(), "Saved", Toast.LENGTH_LONG).show();
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
@@ -104,24 +217,7 @@ public class NewDishActivity extends AppCompatActivity implements View.OnClickLi
             }
 
         }
-        if (i == R.id.loadButton) {
-            try {
-                String message;
-                FileInputStream fis = openFileInput("RecipeText");
-                InputStreamReader isr = new InputStreamReader(fis);
-                BufferedReader br = new BufferedReader(isr);
-                StringBuffer sb = new StringBuffer();
-                while ((message = br.readLine()) != null) {
-                    sb.append(message);
-                }
-                textView.setText(sb.toString());
 
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
     }
 
 
@@ -162,30 +258,9 @@ public class NewDishActivity extends AppCompatActivity implements View.OnClickLi
         }
     }
 
+
     @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l)
-    {
-
-
-        String ingredient = adapterView.getItemAtPosition(i).toString();
-
-        try {
-            FileOutputStream fos1 = openFileOutput(file_name1, MODE_PRIVATE);
-            FileOutputStream fos2 = openFileOutput(file_name2, MODE_PRIVATE);
-            FileOutputStream fos3 = openFileOutput(file_name3, MODE_PRIVATE);
-            FileOutputStream fos4 = openFileOutput(file_name4, MODE_PRIVATE);
-            FileOutputStream fos5 = openFileOutput(file_name5, MODE_PRIVATE);
-            fos1.write(ingredient.getBytes());
-            fos2.write(ingredient.getBytes());
-            fos3.write(ingredient.getBytes());
-            fos4.write(ingredient.getBytes());
-            fos5.write(ingredient.getBytes());
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
 
     }
 
